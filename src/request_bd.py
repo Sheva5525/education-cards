99295ddf34312204a8b7a4baffa81a5dc900ea9d
pbalykov <p.d.balykov@gmail.com>
2023-10-25 15:24:09 +0300

    fix requst_bd

--- src/request_bd.py
+++ src/request_bd.py
@@ -6,11 +6,12 @@ class reques_bd:
         self.cur = self.conn.cursor()
     
     def exec_commit(self, string, parameters = ()):
-        self.cur.execute(string, parameters)
+        print(self.cur.execute(string, parameters).fetchone())
         self.conn.commit()
 
-    def select(self, name, value = NULL id_value = None)->int:
-        str_select = "SELECT {} FROM {};".format(id_value if ( id_value ) else "*", name)
+    def select(self, name, where = None, id_value = None)->int:
+        str_select = "SELECT {} FROM {};".format(id_value if ( id_value ) else "*", name) 
+        str_select += "WHERE " + where if where else ''
         self.exec_commit(str_select)
     
     def insert(self, name, value):
@@ -18,13 +19,17 @@ class reques_bd:
         self.exec_commit(str_insert, value)
 
     def creat_table(self, name, parameters):
-        str_crate = "CREATE TABLE " + name + "(" + parameters + ");"
-        self.exec_commit(str_crate)
-
+        try:
+            str_crate = "CREATE TABLE " + name + "(" + parameters + ");"
+            self.exec_commit(str_crate)
+        except sqlite3.OperationalError:
+           pass
+            
     def creat_value(len_value):
        return "(" + ("?, " * len_value)[:-2] + ")"
-        
-        
+    
+    def __del__(self):
+        self.conn.close()
         
     
 class bd_user(reques_bd):
@@ -35,4 +40,5 @@ class bd_user(reques_bd):
          self.creat_table(self.name_table, str_atribut)
 
      def add_key(self, id_user, login, password):
-         self.insert(self.name_table, (id_user, login, password)) 
+         
+         self.insert(self.name_table, (id_user, login, password))
