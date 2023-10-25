96ee3d712aa0651a7e90ab5644ea0143df8380f4
Sheva <sheva@sheva.host>
2023-10-25 14:49:17 +0300

    Добавлена страница регистрации

--- script/js/main.js
+++ script/js/main.js
@@ -1,4 +1,16 @@
 function sendJSON() {
+
+    if (passwordInput.value != passwordOut.value) {
+        alert("Пароли не совпадают");
+        return;
+    }
+
+    if (passwordInput.value.length < 7) {
+        alert("Пароль меньше семи символов");
+        return;
+    }
+
+
     // с помощью jQuery обращаемся к элементам на странице по их именам
     let name = document.querySelector('#name');
     let lastname = document.querySelector('#lastname');
@@ -21,7 +33,52 @@ function sendJSON() {
       }
     };
     // преобразуем наши данные JSON в строку
-    var data = JSON.stringify({ "name": name.value, "lastname": lastname.value });
+    var data = JSON.stringify({ "login": loginInput.value, "password": passwordInput.value });
     // когда всё готово, отправляем JSON на сервер
     xhr.send(data);
+}
+
+function qwe() {
+    console.log("Login: " + loginInput.value);
+    console.log("Password: " + passwordInput.value);
+    console.log("Type password: " + typeof passwordInput.value);
+}
+
+function show_hide_password(target){
+
+    var input = document.getElementById('passwordInput');
+
+    if (input.getAttribute('type') == 'password') {
+
+        target.classList.add('view');
+
+        input.setAttribute('type', 'text');
+
+    } else {
+
+        target.classList.remove('view');
+
+        input.setAttribute('type', 'password');
+
+    }
+
+    input = document.getElementById('passwordOut');
+
+    if (input.getAttribute('type') == 'password') {
+
+        target.classList.add('view');
+
+        input.setAttribute('type', 'text');
+
+    } else {
+
+        target.classList.remove('view');
+
+        input.setAttribute('type', 'password');
+
+    }
+
+
+    return false;
+
 }
