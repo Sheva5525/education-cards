b8619e3990ebd25a967721eee461bb2311d8fe56
pbalykov <p.d.balykov@gmail.com>
2023-10-29 17:55:24 +0300

    add server post reques

--- src/server.py
+++ src/server.py
@@ -15,11 +15,22 @@ class server(http.server.BaseHTTPRequestHandler):
             self.send_header('Content-Length', len(open('../script/js/main.js','rb').read()))
             self.end_headers()
             self.wfile.write(open('../script/js/main.js','rb').read())
+    
+    def do_POST(self):
+        html = "Все ок\n"
+        if ( self.path == '/regist' ) :
+            l = int(self.headers.get('Content-Length'))
+            l = self.rfile.read(l)
+            print(l.decode())
+            self.send_response(200)
+            self.send_header('Content-Type', 'javascript/js; charset=utf-8')
+            self.send_header('Content-Length', len(html))
+            self.end_headers()
+            self.wfile.write(html.encode())
+
 
 class ThreadingHTTPServer(socketserver.ThreadingMixIn, http.server.HTTPServer):
     pass
 
 if __name__ == '__main__':
     ThreadingHTTPServer(('', 17957), server).serve_forever()
-        
-
